40f5b0ab3321e296d84a661b70e9068ead1ad034
mcodnjs <codnjs8989@gmail.com>
2023-05-01 03:34:57 +0900

    feat: 1주차 - 라온

--- 1주차/PGS_교점에별만들기_라온.py
+++ 1주차/PGS_교점에별만들기_라온.py
@@ -1,13 +1,56 @@
-def solution(s, n):
-    answer = ''
-    for w in list(s):
-        if w == " ":
-            answer += w
+## 최소, 최대 x y 구하는 로직에서 주석처럼 하면 시간초과남 ;
+
+def solution(lines):
+    answer = []      
+    dots = []
+    
+    for i in range(len(lines)):
+        for line in lines[i+1:]:
             
-        elif 'A' <= w <= 'Z':
-            answer += chr(ord('A') + (ord(w) - ord('A') + n) % 26)
+            a, b, e = lines[i]
+            c, d, f = line
+            
+            x_up = b*f - e*d
+            y_up = e*c - a*f
+            xy_under = a*d - b*c
+            
+            if xy_under != 0:
+                x = x_up / xy_under
+                y = y_up / xy_under
+                
+                if isInts(x, y):
+                    dots.append((int(x),int(y)))
+
+    min_x = 1000
+    min_y = 1000
+    max_x = -1000
+    max_y = -1000
+
+    dots = list(set(dots))
+    
+    x_dot_list = [dot[0] for dot in dots]
+    y_dot_list = [dot[1] for dot in dots]
+    min_x, max_x = min(x_dot_list), max(x_dot_list)
+    min_y, max_y = min(y_dot_list), max(y_dot_list)
+    
+    # for x, y in dots:
+    #     min_x = min(x, min_x)
+    #     min_y = min(y, min_y)
+    #     max_x = max(x, max_x)
+    #     max_y = max(y, max_y)
+    
+    x_range = max_x - min_x + 1
+    y_range = max_y - min_y + 1
+
+    graph = [["." for _ in range(min_x, max_x+1)] for _ in range(max_y, min_y-1,-1)]
+    
+    for x, y in dots:
+        graph[abs(y - min_y)][abs(x - min_x)] = "*"
+    
+    for g in graph:
+        answer.append("".join(g))
+    return answer[::-1]
+
+def isInts(a, b):
+    return int(a) == a and int(b) == b
         
-        elif 'a' <= w <= 'z':
-            answer += chr(ord('a') + (ord(w) - ord('a') + n) % 26)
-        
-    return answer
