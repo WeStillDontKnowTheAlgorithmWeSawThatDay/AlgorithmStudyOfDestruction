fdec81b653569648aef9487224f3895991b6473a
jjongwa <troas96@naver.com>
2023-05-03 09:52:11 +0900

    feat: 2주차 - 타겟넘버

---
+++ 2주차/PGS_타겟넘버_디노.py
@@ -0,0 +1,13 @@
+def solution(numbers, target):
+    answer = 0
+    ansList = [0]
+    for num in numbers:
+        tmpList = []
+        for a in ansList:
+            tmpList.append(a+num)
+            tmpList.append(a-num)
+        ansList = tmpList
+    for a in ansList:
+        if a == target:
+            answer += 1
+    return answer
