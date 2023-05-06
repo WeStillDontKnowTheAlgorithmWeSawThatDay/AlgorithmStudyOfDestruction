cb7ef479c8cc10ca9c1392189a07bdd603210d65
wugawuga <dnr3655@naver.com>
2023-05-06 14:22:23 +0900

    feat: 2주차 - 우가

---
+++ 2주차/PGS_입국심사_우가.py
@@ -0,0 +1,18 @@
+def solution(n, times): 
+    answer = 0
+    left, right = 1, max(times) * n
+
+    while left <= right:
+        mid = (left + right) // 2
+        people = 0
+        for time in times:
+            people += mid // time
+            if people >= n:
+                break
+        if people >= n:
+            answer = mid
+            right = mid - 1
+        elif people < n:
+            left = mid + 1
+
+    return answer
