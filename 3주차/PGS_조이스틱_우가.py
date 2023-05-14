aa6ee1011c4c5065b157bf73017e9a9825578f9d
wugawuga <dnr3655@naver.com>
2023-05-14 19:50:16 +0900

    feat: 3주차 - 우가

--- 3주차/PGS_조이스틱_우가.py
+++ 3주차/PGS_조이스틱_우가.py
@@ -1,28 +1,16 @@
 def solution(name):
-    answer = 0
+    count = 0
+    size = len(name)
+    min_move = size - 1
     
-    a = ""
-    for n in range(2):
-        a += name
-
-    count, countA, countB = 0, 0, 0
-    length = len(name)
-    for i in range(length):
-        if a[i + length] != "A":
-            현재마이너스에이 = ord(a[i + length]) - ord("A")
-            제트마이너스현재 = ord("Z") - ord(a[i + length]) + 1
-            count += min(현재마이너스에이, 제트마이너스현재)
-            countA = max(countA, i)
-
-            
-    abc = ""
-    for i in range(1, len(name)):
-        abc+=name[i]
-    
-    k = "".join(reversed(abc))
-    for i in range(len(k)):
-        if a[i] != "A":
-            countB = max(countB, i + 1)
-
-    #48.1점 3일박음
-    return count + min(countA, countB)
+    for idx, char in enumerate(name):
+        count += min(ord(char) - ord('A'), ord('Z') - ord(char) + 1)
+        
+        next_idx = idx + 1
+        while next_idx < size and name[next_idx] == 'A':
+            next_idx += 1
+        
+        min_move = min([min_move, 2 * idx + size - next_idx, idx + 2 * (size - next_idx)])
+        
+    count += min_move
+    return count
