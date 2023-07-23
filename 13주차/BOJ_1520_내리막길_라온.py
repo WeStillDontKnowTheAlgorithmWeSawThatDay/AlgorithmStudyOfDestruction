24cc4845e5ce94586ec6d48512d6912ddacd7abd
mcodnjs <codnjs8989@gmail.com>
2023-07-24 00:58:34 +0900

    feat: 13주차 - 라온

--- 13주차/BOJ_1520_내리막길_라온.py
+++ 13주차/BOJ_1520_내리막길_라온.py
@@ -1,31 +1,34 @@
+## **
+
 from collections import deque
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**7)
 
 m, n = map(int, input().split())
 maps = [list(map(int, input().split())) for _ in range(m)]
 
-ans = 0
-
-# visited =[[0] * n for _ in range(m)]
-# queue = deque()
-# queue.append((0, 0))
-
-def func(answer, stack):
-    i, j = stack[-1]
+def dfs(stack):
+    if not stack:
+        return 0
+    i, j = stack.pop()
     if (i, j) == (m-1, n-1):
-        # print(stack)
-        answer += 1
-        return answer
+        return 1
+    answer = 0
     for dir in dirs:
         x, y = i + dir[0], j + dir[1]
         if 0 <= x < m and 0 <= y < n and maps[x][y] < maps[i][j]:
-            if (x, y) not in stack:
+            if dp[x][y] >= 0:
+                answer += dp[x][y]
+            else:
                 stack.append((x, y))
-                # print(stack)
-                answer = func(answer, stack)
-    stack.pop()
+                answer += dfs(stack)
+    dp[i][j] = answer
     return answer
-
+    
 
 stack = [(0, 0)]
 dirs = [(0, 1), (0, -1), (1, 0), (-1, 0)]
-print(func(ans, stack))
+dp = [[-1]* n for _ in range(m)]
+dp[0][0] = 0
+print(dfs(stack))
