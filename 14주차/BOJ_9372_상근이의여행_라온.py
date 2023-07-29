fa029b51cc1706aee63f9ba21208fbfdcbda7ddb
mcodnjs <codnjs8989@gmail.com>
2023-07-30 03:32:25 +0900

    feat: 14주차 - 라온

---
+++ 14주차/BOJ_9372_상근이의여행_라온.py
@@ -0,0 +1,8 @@
+import sys
+input = sys.stdin.readline
+t = int(input())
+for _ in range(t):
+    n, m = map(int, input().split())
+    for _ in range(m):
+        a, b = map(int, input().split())
+    print(n-1)
---
+++ 14주차/BOJ_9655_돌게임_라온.py
@@ -0,0 +1,5 @@
+n = int(input())
+if n%2 == 0:
+    print("CY")
+else:
+    print("SK")
